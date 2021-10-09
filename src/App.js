e662fb0de30b31d09900c1ea95c16e8c60d192c8
Angshu1997 <arc290897@gmail.com>
2021-10-10 00:32:10 +0530

    Adding whole project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Photo from './components/Photo';
+import Data from './components/Data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+	constructor(){
+		super();
+		this.state={
+			searchfield:'',
+			data:[]
+		}
+	}
+
+	onSearch=(event)=>{
+
+		this.setState({searchfield:event.target.value});
+
+	}
+
+	onSubmit=()=>{
+
+		if(this.state.searchfield ===''){
+			alert("Empty input");
+		}
+
+		fetch(`https://pixabay.com/api/?key=23662676-e53458dd0ffdeedaca1e79e2a&q=${this.state.searchfield}&image_type=photo`)
+		.then(resp=>{
+			return resp.json();
+		})
+		.then(users=>{
+			this.setState({data:users.hits});
+			console.log(this.state.data);
+		})
+       
+		.catch((error)=>{
+			console.log(error);
+		})
+
+	}
+    
+
+    onLoad=()=>{
+    	window.location.reload();
+    }
+
+
+
+
+
+	render(){
+		return(
+			<div>
+			<h1 className="b">Photo Gallery</h1>
+			<Photo onSearch={this.onSearch} onLoad={this.onLoad} onSubmit={this.onSubmit}/>
+			<Data data={this.state.data}/>
+			</div>
+
+			);
+	}
 }
+
 
 export default App;
